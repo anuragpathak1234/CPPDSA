02203ee63a47e2d78d21a085907145ceb7f22480
Chhotu Pathak <chhotupathak9102@gmail.com>
2026-09-07 10:21:57 +0530

    learn about dice in recursive way

---
+++ Dice.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+
+
+void dice(string p, int target){
+
+  if(target == 0){
+    cout<<p<<endl;
+    return ;
+  }
+
+  for(int i = 1; i <= 6 && i <= target; i++){
+
+    dice(p + to_string(i), target - i);
+  }
+
+
+}
+
+
+
+int main(){
+
+  dice("",4);
+  return 0;
+
+}
